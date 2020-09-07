89eed8ad1f6a5fb7ff4119b613f72197e7023ac6
Katie Lamber02 <katielamber02@gmail.com>
2020-09-07 12:53:19 +0300

    config added

--- pages/register.js
+++ pages/register.js
@@ -1,6 +1,10 @@
 import Layout from "../components/Layout"
 import { useState } from 'react'
 import axios from 'axios'
+import { API, DOMAIN } from '../config'
+
+
+// console.log('API', API, DOMAIN)
 
 const Register = () => {
 
@@ -17,30 +21,79 @@ const Register = () => {
 
         setState({ ...state, [name]: event.target.value, error: '', success: '', buttonText: 'Register' })
     }
-    const handleSubmit = event => {
+
+    const handleSubmit = async event => {
+        setState({
+            ...state, buttonText: 'Register processing...'
+        })
         event.preventDefault()
         console.table({ name, email, password })
-        axios.post(`http://localhost:8000/api/register`, {
-            name: name,
-            email: email,
-            password: password
-        })
-            .then(response => console.log(response))
-            .catch(error => console.log(error))
+        try {
+            const response = await axios.post(`${API}/register`, {
+                name: name,
+                email: email,
+                password: password
+            })
+            console.log(response)
+            setState({
+                ...state,
+                name: '',
+                email: '',
+                password: '',
+                success: response.data.message,
+                // res.json({
+                //         message: `Email has been sent to ${email}`
+                //         // {message: "Email has been sent to katielamber02@gmail.com"}
+                //     })
+                buttonText: 'Submitted'
+            })
+        } catch (errror) {
+            setState({ ...state, buttonText: 'Register', error: error.data.error })
+        }
     }
+
+    // const handleSubmit = event => {
+    //     setState({
+    //         ...state, buttonText: 'Register processing...'
+    //     })
+    //     event.preventDefault()
+    //     console.table({ name, email, password })
+    //     axios.post(`http://localhost:8000/api/register`, {
+    //         name: name,
+    //         email: email,
+    //         password: password
+    //     })
+    //         .then(response => {
+    //             setState({
+    //                 ...state,
+    //                 name: '',
+    //                 email: '',
+    //                 password: '',
+    //                 success: response.data.message,
+    //                 // res.json({
+    //                 //         message: `Email has been sent to ${email}`
+    //                 //         // {message: "Email has been sent to katielamber02@gmail.com"}
+    //                 //     })
+    //                 buttonText: 'Submitted'
+    //             })
+    //         })
+    //         .catch(error => {
+    //             setState({ ...state, buttonText: 'Register', error: error.data.error })
+    //         })
+    // }
 
 
     const registerFrom = () => (
         <form onSubmit={handleSubmit}>
             <div className="form-group">
-                <input value={name} onChange={handleChange('name')} type="text" className="form-control" placeholder="Type your name" />
+                <input value={name} onChange={handleChange('name')} type="text" className="form-control" placeholder="Type your name" required />
             </div>
             <div className="form-group">
-                <input value={email} onChange={handleChange('email')} type="email" className="form-control" placeholder="Type your email" />
+                <input value={email} onChange={handleChange('email')} type="email" className="form-control" placeholder="Type your email" required />
             </div>
 
             <div className="form-group">
-                <input value={password} onChange={handleChange('password')} type="password" className="form-control" placeholder="Type your password" />
+                <input value={password} onChange={handleChange('password')} type="password" className="form-control" placeholder="Type your password" required />
             </div>
             <div className="form-group">
                 <button className="btn btn-outline-primary">{buttonText}</button>
@@ -48,6 +101,8 @@ const Register = () => {
         </form>
     )
     return <Layout>
+        {success && success}
+        {error && error}
         <div className="col-md-6 offset-md-3">
             <h1>Register</h1>
             <br />
@@ -59,3 +114,20 @@ const Register = () => {
 }
 
 export default Register
+
+
+
+// sendRegistrationEmail.then(data => {
+//     console.log('data submitted to SES:', data)
+//     res.json({
+//         message: `Email has been sent to ${email}`
+//         // {message: "Email has been sent to katielamber02@gmail.com"}
+//     })
+// }).catch(error => {
+//     console.log('error when data submitted to SES:', error)
+//     res.json({
+//         error: `Email could not be verified`
+//     })
+//     // {error: "Email could not be verified"}
+//     // error: "Email could not be verified"
+// })
